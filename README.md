dc2391422207b6c30d90819fcfceb8e808291cf3
Nicolas Vanhaezebrouck <nvanhaezebrouck@consomind.com>
2023-03-23 11:29:43 +0100

    init

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# jwt
-middleware pour jwt de firebase
+# JWT
+Un middleware minimaliste pour firebase/php-jwt
+
+## Installation
+
+Via composer
+```
+composer require veka-server/jwt
+```
+
+## Utiliser le router comme un middleware PSR-15
+```php
+// creation du dispatcher
+$Dispatcher = new VekaServer\Dispatcher\Dispatcher();
+
+// creer le jwt
+$jwt = new \App\classe\JWT(
+                  $config->get('JWT_PRIVATE_KEY'), // private_key
+                  $config->get('JWT_PUBLIC_KEY'), // public_key
+                  'localhost:8000', // iss
+                  3600, // expire time token -- facultatif default = 3600
+                  'RS256', // algorithm de chiffrement -- facultatif default = RS256
+                  'localhost:8000', // aud -- facultatif default = iss
+              );
+
+// ajout le middlewares
+$Dispatcher->pipe($jwt);
+```
---
+++ src/JWT.php
@@ -0,0 +1,165 @@
+<?php
+
+namespace VekaServer\JWT;
+
+use Exception;
+use Firebase\JWT\Key;
+use Nyholm\Psr7\Response;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use stdClass;
+
+class JWT implements MiddlewareInterface
+{
+
+    /**
+     * @var String
+     */
+    private string $private_key;
+    private string $public_key;
+    private string $algorithm;
+    private string $expireTimeToken;
+    private string $iss; // créateur (issuer) du jeton
+    private string $aud; // audience du jeton
+    private array $exclude_url = []; // list url not require token
+
+    public function __construct(String $private_key
+                                ,String $public_key
+                                ,String $iss
+                                ,int $expireTimeToken = 3600
+                                ,String $algorithm = 'RS256'
+                                ,String $aud = null) {
+        $this->private_key = $private_key;
+        $this->public_key = $public_key ;
+        $this->iss = $iss;
+        $this->algorithm = $algorithm;
+        $this->expireTimeToken = $expireTimeToken;
+        $this->aud = $aud ?? $iss;
+    }
+
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler) :ResponseInterface
+    {
+        $authResponse = $this->authorise($request);
+
+        if ($authResponse->getStatusCode() === 200) {
+            return $handler->handle($request);
+        }
+
+        return $authResponse;
+    }
+
+    public function getToken($data = []) :string
+    {
+
+        $time = time();
+        $expireAt = $time + $this->expireTimeToken;
+
+        $payload = [
+            'iss' => $this->iss,
+            'aud' => $this->aud,
+            'iat' => time(), // time of token issued at
+            'nbf' => $time + 1, //not before in seconds
+            'exp' => $expireAt, // expire time of token in seconds
+            'data' => $data
+        ];
+
+        return \Firebase\JWT\JWT::encode($payload, $this->private_key, $this->algorithm);
+    }
+
+    public function decode($jwt) :stdClass
+    {
+        \Firebase\JWT\JWT::$leeway = 10;
+        return \Firebase\JWT\JWT::decode($jwt, new Key($this->public_key, $this->algorithm));
+    }
+
+    private static function cleanPath($url){
+
+        $len = strlen($url);
+        if ($len <= 0) {
+            return '';
+        }
+
+        // Fix missing begin-/
+        if ($url[0] != '/') {
+            $url = '/' . $url;
+        }
+
+        // Fix trailing /
+        if ($len > 1 && $url[$len - 1] == '/') {
+            $url = substr($url, 0, -1);
+        }
+
+        return $url;
+    }
+
+    private function authorise(ServerRequestInterface $request) :Response
+    {
+        $uri = $request->getServerParams()['REQUEST_URI'] ?? '';
+        foreach ($this->exclude_url as $url){
+            if($this->checkURI($url, $uri)){
+                return new Response(200);
+            }
+        }
+
+        try {
+            $authHeader = $request->getServerParams()['HTTP_AUTHORIZATION'] ?? '';
+            if(empty($authHeader)){
+                $temp_header = explode(" ", $authHeader);
+                $jwt = $temp_header[1] ?? '';
+            }
+
+            $this->decode($jwt ?? null);
+
+            $errorCode = 200;
+
+        }catch (Exception $e){
+
+            $jsonBody = [
+                'success' => false,
+                "jwt" => $jwt ?? null,
+                'error' => [
+                    'message' => $e->getMessage(),
+                    'help' => 'Token JWT incorrect or missing. Please add header => Authorization: Bearer [token]',
+                ],
+            ];
+
+            $reason = 'Unauthorized';
+            $jsonBody = json_encode($jsonBody);
+            $errorCode = 401;
+        }
+
+        return new Response($errorCode, [], ($jsonBody ?? null), '1.1', ($reason ?? null) );
+    }
+
+    /**
+     * @param string $regex
+     * @param string $path
+     * @return bool
+     */
+    public function checkURI(string $regex, string $path) :bool
+    {
+        $regex =self::cleanPath($regex);
+        $path =self::cleanPath($path);
+
+        if(empty($regex)){
+            return false;
+        }
+
+        if($regex == $path) {
+            return true;
+        }
+
+        // Prevent @ collision
+        $regex = str_replace('@', '\\@', $regex);
+        $test = preg_match('@^' . $regex . '$@', $path);
+
+        return (bool)($test);
+    }
+
+    public function excludeUrl($url)
+    {
+        $this->exclude_url[] = $url;
+    }
+}
